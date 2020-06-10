24728cece47a3e579894dc8eacba3f73ed1bfeba
Rituja <rituja.dixit@xebia.com>
2020-06-10 18:43:01 +0530

    useState with object-set firstname and lastname example

---
+++ src/components/HookCounterThree.js
@@ -0,0 +1,30 @@
+// useState with object
+// useState does not automatically update and merge the state. 
+// This is a key difference with the setState. setState merges the state automatically
+
+import React, {useState} from 'react'
+
+function HookCounterThree() {
+
+    const [name, setName] = useState({firstName: '', lastName: ''})
+    return (
+        <div>
+            <form>
+                <input 
+                    type="text" 
+                    value={name.firstName} 
+                    onChange={e => setName({ ...name, firstName: e.target.value })}
+                />
+                <input
+                    type="text" 
+                    value={name.lastName} 
+                    onChange={e => setName({ ...name, lastName: e.target.value })}
+                />
+                <h2>Your first name is - {name.firstName}</h2>
+                <h2>Your first name is - {name.lastName}</h2>
+            </form>
+        </div>
+    )
+}
+
+export default HookCounterThree
