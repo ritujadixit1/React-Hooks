cd032f0272814a46cbd43485c400a08d3be57bf1
Rituja <rituja.dixit@xebia.com>
2020-06-10 11:51:12 +0530

    useState Hook-Counter App example

---
+++ src/components/HookCounter.js
@@ -0,0 +1,18 @@
+import React, {useState} from 'react'
+
+function HookCounter() {
+
+    const [count, setCount] = useState(0)
+
+    return (
+        <div>
+            <button onClick={() => setCount(count + 1)}>Count {count}</button>
+        </div>
+    )
+}
+
+export default HookCounter
+
+// Rules of Hooks:-
+// 1. Only call Hooks at the Top Level
+// 2. Only call Hooks from React Functions
