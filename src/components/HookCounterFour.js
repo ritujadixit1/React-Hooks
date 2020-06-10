a16809b0716e16ea6c16f74a5efde9725b03a800
Rituja <rituja.dixit@xebia.com>
2020-06-10 21:15:44 +0530

    useState with array-add a number example

---
+++ src/components/HookCounterFour.js
@@ -0,0 +1,28 @@
+// useState with array
+// New state depends on the previous state value? You can pass a function to the setter function
+
+import React, {useState} from 'react'
+
+function HookCounterFour() {
+    const [items, setItems] = useState([])
+
+    const addItem = () => {
+        setItems([ ...items, {
+            id: items.length,
+            value: Math.floor(Math.random() * 10) + 1
+        } ])
+    }
+
+    return (
+        <div>
+            <button onClick={addItem}>Add a number</button>
+            <ul>
+                {
+                    items.map(item => (<li key={item.id}>{item.value}</li>
+                ))}
+            </ul>
+        </div>
+    )
+}
+
+export default HookCounterFour
