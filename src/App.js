549590c15d67bc2d96f9f473c3bfba2975151684
Rituja <rituja.dixit@xebia.com>
2020-06-28 08:37:25 +0530

    removing .env file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
-import ClassCounterOne from './components/ClassCounterOne';
+// import ClassCounterOne from './components/ClassCounterOne';
 import HookCounterOne from './components/HookCounterOne';
 
 function App() {
   return (
     <div className="App">
-      <ClassCounterOne />
+      {/* <ClassCounterOne /> */}
       <HookCounterOne />
     </div>
   );
